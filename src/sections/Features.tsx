60dfa0a3508f28ca8edd503d76c5509fa57c60b6
aaronjsharp <aaron.sharp2011@gmail.com>
2024-08-15 15:22:46 -0500

    Complete hover animation in features section

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -2,8 +2,10 @@
 
 import productImage from '@/assets/product-image.png';
 
-import { DotLottiePlayer } from '@dotlottie/react-player';
+import { DotLottieCommonPlayer, DotLottiePlayer } from '@dotlottie/react-player';
 import Image from 'next/image';
+import { dot } from 'node:test/reporters';
+import { useRef } from 'react';
 
 const tabs = [
   {
@@ -32,6 +34,34 @@ const tabs = [
   },
 ];
 
+const FeatureTab = (tab: typeof tabs[number]) => {
+  const dotLottieRef = useRef<DotLottieCommonPlayer>(null)
+  const handleTabHover = () => {
+    if (dotLottieRef.current === null) return
+
+    dotLottieRef.current.seek(0)
+    dotLottieRef.current.play()
+  }
+  return (
+    <div
+      key={tab.title}
+      className='border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center lg:flex-1 cursor-pointer'
+      onMouseEnter={handleTabHover}
+    >
+      <div className='h-12 w-12 border border-white/15 rounded-lg inline-flex items-center justify-center'>
+        <DotLottiePlayer ref={dotLottieRef} src={tab.icon} className='h-5 w-5' autoplay />
+      </div>
+
+      <div className='font-medium'>{tab.title}</div>
+      {tab.isNew && (
+        <div className='text-xs rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold'>
+          new
+        </div>
+      )}
+    </div>
+  );
+};
+
 export const Features = () => {
   return (
     <section className='py-20 md:py-24'>
@@ -45,27 +75,16 @@ export const Features = () => {
         </p>
         <div className='mt-10 flex flex-col lg:flex-row gap-3'>
           {tabs.map((tab) => (
-            <div
-              key={tab.title}
-              className='border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center lg:flex-1'
-            >
-              <div className='h-12 w-12 border border-white/15 rounded-lg inline-flex items-center justify-center'>
-                <DotLottiePlayer src={tab.icon} className='h-5 w-5' autoplay />
-              </div>
-
-              <div className='font-medium'>{tab.title}</div>
-              {tab.isNew && (
-                <div className='text-xs rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold'>
-                  new
-                </div>
-              )}
-            </div>
+            <FeatureTab {...tab} key={tab.title} />
           ))}
         </div>
         <div className='border border-white/20 p-2.5 rounded-xl mt-3'>
-          <div className='aspect-video bg-cover border border-white/20 rounded-lg' style={{
-            backgroundImage: `url(${productImage.src})`
-          }} />
+          <div
+            className='aspect-video bg-cover border border-white/20 rounded-lg'
+            style={{
+              backgroundImage: `url(${productImage.src})`,
+            }}
+          />
         </div>
       </div>
     </section>
